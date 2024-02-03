cb8d52802e318eef5df66d4344f625d5712871a2
suman5184 <sumanchowdary.g@gmail.com>
2024-02-03 12:34:29 +0530

    test 2

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+person1=ram 
+person2=rahul
+echo "$person1: Hello $person2, Good Morning"
+echo  "$person2: Hi $person1, Very Good Morning"
+
 echo "Ramesh: Hello suresh, Good Morning"
 echo  "Suresh: Hi Ramesh, Very Good Morning"
