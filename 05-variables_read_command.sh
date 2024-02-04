a98ebcd12d9d9ea87bc027bc029ed814406e9876
suman5184 <sumanchowdary.g@gmail.com>
2024-02-04 10:33:42 +0530

    test

--- 05-variables_read_command.sh
+++ 05-variables_read_command.sh
@@ -2,6 +2,6 @@
 echo "Please enter username:"
 read USERNAME #the value entered above will be automatically attached to USERNAME variable
 echo "Username is :$USERNAME"
- Echo "Please enter Password:"
+echo "Please enter Password:"
  read PASSWORD 
- echo "Password is :$PASSWORD"
+ echo "Password is :$PASSWORD
