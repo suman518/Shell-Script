b2f1342315a3675dfdee780f5ee7d7f838c94b4d
suman5184 <sumanchowdary.g@gmail.com>
2024-02-06 17:25:33 +0530

    Status

--- 07-Array.sh
+++ 07-Array.sh
@@ -3,5 +3,5 @@ echo "First Fruit :${FRUITS[1]}"
 echo "All Fruits :${FRUITS[@]}"
 
 echo "Second Fruit :${FRUITS[1]}"
-Fruitnam=${FRUITS[3]}
+Fruitnam=${FRUITS[2]}
 echo " Variable Fruit name :$Fruitnam"
